3b442b12b46e190630cef0741ac36a5085f41526
Ujah Emmanuel <ujahemmanuel72@gmail.com>
2022-06-19 18:35:35 +0100

    hooking up heroku

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Index page ready');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
